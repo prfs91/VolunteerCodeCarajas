9b672c66bd5c6d28bc95eff95422827ae6ffd3bc
prfs91 <pamellaferreira.si@gmail.com>
2025-04-15 03:02:14 -0300

    fix: corrects error in the save method of the Associado model when generating automatic number for the associated member

--- associados/models.py
+++ associados/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Modelo que representa um associado no sistema
 class Associado(models.Model):
     # DADOS PESSOAIS
     nome = models.CharField(max_length=100)
     nome_social = models.CharField(max_length=100, blank=True)  # Para respeitar identidade de gênero
-    data_nascimento = models.DateField()
+    data_nascimento = models.DateField(default='1900-01-01')
     cpf = models.CharField(max_length=14, unique=True) # CPF do associado – precisa ser único
     rg = models.CharField(max_length=20, blank=True)
     orgao_emissor = models.CharField(max_length=20, blank=True)
@@ -15,20 +17,20 @@ class Associado(models.Model):
     naturalidade = models.CharField(max_length=50, blank=True)
     
     # CONTATO E ENDEREÇO
-    endereco = models.CharField(max_length=200)
-    cep = models.CharField(max_length=10)
+    endereco = models.CharField(max_length=200, default='Endereço não informado')
+    cep = models.CharField(max_length=10, default='00000-000')
     telefone = models.CharField(max_length=15, blank=True) # Telefone é opcional (blank=True)
     email = models.EmailField()
 
     # INFORMAÇÕES SOBRE A DEFICIÊNCIA
-    tipo_deficiencia = models.CharField(max_length=100)
+    tipo_deficiencia = models.CharField(max_length=100, default="Não informado", blank=True)
     cid = models.CharField(max_length=20, blank=True)  # Classificação Internacional de Doenças
     laudo_medico = models.FileField(upload_to='laudos/', blank=True)
     necessidades_acessibilidade = models.TextField(blank=True)
     data_diagnostico = models.DateField(blank=True, null=True)
 
     # DOCUMENTAÇÃO LEGAL
-    numero_associado = models.CharField(max_length=20, unique=True)
+    numero_associado = models.CharField(max_length=20, unique=True, blank=True, null=True)
     data_entrada = models.DateField(auto_now_add=True)
     status = models.CharField(max_length=20, default='ativo')  # Ativo, Inativo etc.
     comprovante_residencia = models.FileField(upload_to='comprovantes/', blank=True)
@@ -54,6 +56,23 @@ class Associado(models.Model):
     # Data de entrada é definida automaticamente na criação
     data_entrada = models.DateField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        if not self.numero_associado:  # Se o número do associado não foi preenchido
+            ano_atual = str(timezone.now().year)  # Ano da data de entrada (você pode ajustar conforme necessário)
+            ultimo_numero = Associado.objects.filter(data_entrada__year=timezone.now().year).order_by('numero_associado').last()
+            
+            if ultimo_numero and ultimo_numero.numero_associado:
+                # Se houver um registro, pega o último número do associado e incrementa
+                ultimo_codigo = int(ultimo_numero.numero_associado.split('-')[1]) + 1
+            else:
+                # Caso não haja registros, inicia o código a partir de 1
+                ultimo_codigo = 1
+                
+            numero_associado = f"ASSOC-{ano_atual}-{ultimo_codigo:04d}"
+            self.numero_associado = numero_associado
+            
+        super(Associado, self).save(*args, **kwargs)  # Chama o método save original
+    
     # Representação do associado (exibido no admin e em listas)
     def __str__(self):
         return self.nome
