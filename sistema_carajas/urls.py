93458fb8138ad12a09f0f41cb41e77ecc9189560
prfs91 <pamellaferreira.si@gmail.com>
2025-04-18 02:30:17 -0300

    🎨 style (urls): improve URL routing structure and readability in urls.py
    This file defines the main URL routing for the 'sistema_carajas' Django project. It maps paths to views, including the home view and routes from the 'associados' app. The recent changes focused on improving readability and formatting, such as clearer comments and code structure, without affecting the logic.

--- sistema_carajas/urls.py
+++ sistema_carajas/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from sistema_carajas import views #Isso assume que o views.py está dentro da pasta sistema_carajas, que é onde fica o urls.py principal.
-from django.urls import path, include
+from django.urls import include, path
+
+from sistema_carajas import \
+    views  # Isso assume que o views.py está dentro da pasta sistema_carajas, que é onde fica o urls.py principal.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
